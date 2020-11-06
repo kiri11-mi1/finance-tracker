12389bcfbd873a661d8db0843d3c9756519f91fb
kiri11_mi1 <kirill.mil.0310@mail.ru>
2020-11-06 18:55:52 +0700

    Написал методы работы с базой данных

--- db.py
+++ db.py
@@ -5,23 +5,46 @@ import sqlite3
 db = sqlite3.connect('finance.db')
 cursor = db.cursor()
 
+
 def insert(table, column_values):
     '''Добавление в таблицу'''
-    pass
+    columns = ', '.join( column_values.keys() )
+    values = [ tuple(column_values.values()) ]
+    placeholders = ', '.join( "?" * len(column_values.keys()) )
+    return placeholders
+    cursor.executemany(
+        f"INSERT INTO {table}"
+        f"({columns})"
+        f"VALUES ({placeholders})",
+        values)
+    db.commit()
 
 
-def get_all(table, columns):
-    '''Получение данных с таблицы'''
-    pass
+
+def fetchall(table, columns):
+    '''Получение данных с таблицы''' 
+    columns_joined = ', '.join(columns)
+    cursor.execute(f'SELECT {columns_joined} FROM {table}')
+    rows = cursor.fetchall()
+    result = []
+    for row in rows:
+        dict_row = {}
+        for index, column in enumerate(columns):
+            dict_row[column] = row[index]
+        result.append(dict_row)
+    return result
+
 
 
 def delete(table, row_id):
     '''Удаление строки из таблицы'''
-    pass
+    row_id = int(row_id)
+    cursor.execute(f"delete from {table} where id={row_id}")
+    db.commit()
 
 
 def get_cursor():
-    pass
+    return cursor
 
 
 def init_db():
